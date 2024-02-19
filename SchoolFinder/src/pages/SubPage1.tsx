f099afbf83eb6c83797b58cb76f1efc2b641ec47
Mateusz Jarzębski <mjarzebski@freeportmetrics.com>
2024-02-19 19:29:52 +0100

    added routes and pages mock

---
+++ SchoolFinder/src/pages/SubPage1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubPage1: React.FC = () => {
+  return (
+    <div>
+      <h1>SubPage 1</h1>
+    </div>
+  );
+};
+
+export default SubPage1;
---
+++ SchoolFinder/src/pages/SubPage2.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubPage2: React.FC = () => {
+  return (
+    <div>
+      <h1>SubPage 2</h1>
+    </div>
+  );
+};
+
+export default SubPage2;
