aece63c0e0111fe8c6e34db775d087508b77b8ee
Mateusz Jarzębski <mjarzebski@freeportmetrics.com>
2024-03-16 11:16:58 +0100

    first antd work formSection

--- SchoolFinder/src/components/CalculatorPage/Calculator.tsx
+++ SchoolFinder/src/components/CalculatorPage/Calculator.tsx
@@ -1,78 +1,87 @@
 import React from "react";
 import "./Calculator.css";
-import { Checkbox, Input } from "@nextui-org/react";
+import { Button, Checkbox, Form, Input } from "antd";
 
 const Calculator: React.FC = () => {
+  function onFinish(values: any): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <>
-      <div className="mainCalculatorContainer">
+      <Form
+        labelCol={{ span: 14 }}
+        wrapperCol={{ span: 16 }}
+        className="formStyle"
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+        colon={false}
+      >
         <div className="sectionDivider">
-          <h1>1.Oblicz swoje punkty</h1>
+          <h1>1. Oblicz swoje punkty</h1>
         </div>
+
         <div className="formSection">
-          <Checkbox className="checkboxStyle" color="secondary">
+          <Checkbox className="checkboxStyle">
             Wiem już ile będę mieć punktów
           </Checkbox>
           <div className="subSectionDivider">
             <p>Oceny na świadectwie</p>
           </div>
-          <div className="subSection">
-            <div className="formRow">
-              <p className="mr-24">język polski</p>
-              <Input
-                className="mr-6"
-                type="number"
-                max={100}
-                min={0}
-                label="Wynik"
-                placeholder="0"
-                endContent={
-                  <div className="pointer-events-none flex items-center">
-                    <span className="text-default-400 text-small">%</span>
-                  </div>
-                }
-              />
-              <Input type="text" label="Uzyskane punkty" />
-            </div>
+          {/* <div className="subSection"> */}
 
-            <div className="formRow">
-              <p className="mr-24">język polski</p>
-              <Input
-                className="mr-6"
-                type="number"
-                max={100}
-                min={0}
-                label="Wynik"
-                placeholder="0"
-                endContent={
-                  <div className="pointer-events-none flex items-center">
-                    <span className="text-default-400 text-small">%</span>
-                  </div>
-                }
-              />
-              <Input type="text" label="Uzyskane punkty" />
+          <Form.Item
+            label="język polski"
+            name="jezykPolski"
+            rules={[{ required: true, message: "Proszę podaj wynik" }]}
+            labelCol={{ span: 8 }} // Set a fixed width for the label column
+            className="customFormItem"
+            labelAlign="left"
+          >
+            <div className="inputContainer">
+              <Input className="inputStyle" placeholder="Wynik [%]" />
+              <Input className="inputStyle" placeholder="Wynik [%]" />
             </div>
+          </Form.Item>
 
-            <div className="formRow">
-              <p className="mr-24">język polski</p>
-              <Input
-                className="mr-6"
-                type="number"
-                max={100}
-                min={0}
-                label="Wynik"
-                placeholder="0"
-                endContent={
-                  <div className="pointer-events-none flex items-center">
-                    <span className="text-default-400 text-small">%</span>
-                  </div>
-                }
-              />
-              <Input type="text" label="Uzyskane punkty" />
+          <Form.Item
+            label="język polski"
+            name="jezykPolski"
+            rules={[{ required: true, message: "Proszę podaj wynik" }]}
+            labelCol={{ span: 8 }} // Set a fixed width for the label column
+            className="customFormItem"
+            labelAlign="left"
+          >
+            <div className="inputContainer">
+              <Input className="inputStyle" placeholder="Wynik [%]" />
+              <Input className="inputStyle" placeholder="Wynik [%]" />
             </div>
-          </div>
+          </Form.Item>
+
+          <Form.Item
+            label="język obcy"
+            name="jezykObcy"
+            rules={[{ required: true, message: "Proszę podaj wynik" }]}
+            labelCol={{ span: 8 }} // Set a fixed width for the label column
+            className="customFormItem"
+            labelAlign="left"
+          >
+            <div className="inputContainer">
+              <Input className="inputStyle" placeholder="Wynik [%]" />
+              <Input className="inputStyle" placeholder="Wynik [%]" />
+            </div>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
         </div>
-      </div>
+        {/* </div> */}
+      </Form>
     </>
   );
 };
